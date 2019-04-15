c840f5e616c3993ed50afc10e8c5bf0f35e2cbe9
James Jones <james.jones@phoenixweb.com.au>
2019-04-16 04:58:37 +0930

    setup composer.json for Bootstrap and jQuery and rejigged scss, css and js to suit

--- public_html/include/header.php
+++ public_html/include/header.php
@@ -3,12 +3,11 @@
     <html>
     <head>
         <title><?php echo ucfirst( $script_filename ) . ' - ' . SYSTEM_TITLE; ?></title>
-        <link rel="stylesheet" type="text/css" href="vendor/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="css/styles.min.css">
         <link rel="stylesheet" type="text/css" href="css/datepicker.min.css">
         <link rel="stylesheet" type="text/css" href="css/fonts.css">
-        <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-        <script type="text/javascript" src="vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/jquery.tablesorter.js"></script>
         <script type="text/javascript" src="js/mousetrap.min.js"></script>
         <script type="text/javascript" src="js/mousetrap-global-bind.min.js"></script>
